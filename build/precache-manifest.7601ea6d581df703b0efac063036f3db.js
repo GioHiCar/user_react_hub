668ee09b600090090c8b5b6193f55cee66af3a3e
giohicar <giohicar@gmail.com>
2022-07-05 14:24:51 -0500

    new save

---
+++ build/precache-manifest.7601ea6d581df703b0efac063036f3db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0b3ec68866b2d138fbb02ee0787a73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9edd5308f246c7ba61ec",
+    "url": "/static/css/main.5d307e69.chunk.css"
+  },
+  {
+    "revision": "208980bb86e8f844b5ef",
+    "url": "/static/js/2.3408c895.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3408c895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edd5308f246c7ba61ec",
+    "url": "/static/js/main.655afeac.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.655afeac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56e041857a0bf600d3",
+    "url": "/static/js/runtime-main.3715f0a4.js"
+  }
+]);
